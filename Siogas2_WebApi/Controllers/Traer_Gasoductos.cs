7a2b16a812dea3af2d458838cba02b41be9959e1
Paco <114266465+lucho-netizen@users.noreply.github.com>
2023-09-14 17:00:33 -0500

    Beta 1.8.0

---
+++ Siogas2_WebApi/Controllers/Traer_Gasoductos.cs
@@ -0,0 +1,11 @@
+﻿using Siogas2_webapi.Models;
+
+namespace Siogas2_WebApi.Controllers
+{
+    internal class Traer_Gasoductos
+    {
+        public string? message { get; set; }
+        public bool success { get; set; }
+        public List<Gasoducto>? datos { get; set; }
+    }
+}
